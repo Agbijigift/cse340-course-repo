b81a49ff516253ba1314924266b86ac61d5fa12f
Gift Agbiji <giftagbiji8@gmail.com>
2026-03-13 23:00:09 +0100

    done

--- src/models/setup.sql
+++ src/models/setup.sql
@@ -14,3 +14,9 @@ VALUES
 ('BrightFuture Builders', 'A nonprofit focused on improving community infrastructure through sustainable construction projects.', 'info@brightfuturebuilders.org', 'brightfuture-logo.png'),
 ('GreenHarvest Growers', 'An urban farming collective promoting food sustainability and education in local neighborhoods.', 'contact@greenharvest.org', 'greenharvest-logo.png'),
 ('UnityServe Volunteers', 'A volunteer coordination group supporting local charities and service initiatives.', 'hello@unityserve.org', 'unityserve-logo.png');
+
+
+app.get('/projects', async (req, res) => {
+    const projects = await getAllProjects();
+    res.render('projects', { projects });
+});
